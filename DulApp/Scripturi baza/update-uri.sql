88c22c190566ef856e481304465a560e444051d1
alecsandru29 <alexandru.trifan@info.uaic.ro>
2017-05-18 15:48:47 +0300

    delete

--- DulApp/Scripturi baza/update-uri.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE UserData
-DROP COLUMN Varsta;
-
-ALTER TABLE UserData
-ADD Data_nastere VARCHAR2(20 BYTE);
-
-UPDATE userdata
- set Data_Nastere=CONCAT (Cast(RAND()*(28-1)+1 as int),'-',Cast(RAND()*(12-1)+1 as int),'-',Cast(RAND()*(2001-1960)+1960 as int));
