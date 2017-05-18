326f5664e09ad907099cbd02fdf93ca405d94907
AlexandraMihalcea <alexandra.mihalcea@info.uaic.ro>
2017-05-18 20:35:34 +0300

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -1,12 +1,18 @@
+<?php session_start(); ?>
 <html>
 <head>
-<link rel="icon" type="image/png" href="image/login.png">
+<link rel="icon" type="image/png" href="image/icon.png">
 <link rel="stylesheet" type="text/css" href="css/login.css">
 <title>Login</title>
 </head>
 <body>
+<?php
+session_unset();
+session_destroy(); 
+?>
 <div class="log_win">
 <form action="/DulApp/Login.php" method="post">
+		<input type="hidden" value="0" name="r">
 		<input type="text" name="username" placeholder="Username" required><br>
 		<input type="password" name="password" placeholder="Password"  required pattern="(?=.*\d).{7,}"
 		 title="Must contain at least one number and at least 7 or more characters"><br>
